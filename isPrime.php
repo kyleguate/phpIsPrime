e9157d486edb0b7e81fbf35d26fffe1e18c140df
Kyle Klein <kyleguate@gmail.com>
2014-01-20 08:02:05 -0700

    Create isPrime.php
    Uncomment out the testing section to try it out, or write your own test

---
+++ isPrime.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Determines if a number is prime
+ * 
+ * @param int - whole number
+ * @return boolean - true if is prime, false if not 
+ * @author: kyleguate@gmail
+ */
+function isPrime($number){
+	
+	if ($number % 2 === 0){
+		return false;
+	}
+	
+	$i = 3;
+	$currentQuotient = round($number/$i);
+	
+	for ($i = 3; $i <= $currentQuotient; $i+=2){
+		
+		if ($number % $i === 0){ 
+			return false; 
+		}
+		$currentQuotient = round($number/$i);
+	}
+	return true;
+}
+
+/* Testing */
+//for ($i = 1; $i <= 100; $i++){
+//	echo $i . ': ' . isPrime($i) . '<br />';
+//} 
